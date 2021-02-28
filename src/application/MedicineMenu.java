85eeb5f909851a303d9d1de68e5cf2e2b7f9d56e
J.ludwig <01josephludwig@gmail.com>
2021-02-27 16:45:27 -0800

    Working on it

--- src/application/MedicineMenu.java
+++ src/application/MedicineMenu.java
@@ -1,7 +1,21 @@
 package application;
 
 public class MedicineMenu {
+
   public static void showMedicines() {
+ 
+	  String selection = "2";
+	  
+	  do {
+		  printMenu();
+		  selection = scanner.nextline();
+		  
+		  if (selection.equals("2")) {
+			  
+			  displayMeedicine
+			  }
+		  while (!selection.equals("-1"));
+	  }
   }
   
   public static void createMedicine() {
--- src/application/RegimenMenu.java
+++ src/application/RegimenMenu.java
@@ -1,7 +1,13 @@
 package application;
 
+import java.util.Scanner;
+
 public class RegimenMenu {
+	
+	private Scanner scanner = new Scanner(System.in);
+	
   public static void showUserRegimen() {
+	 
   }
   
   public static void createRegimen() {
